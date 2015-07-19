9c6865d47636f92a2548bc777a4b4d63ecb86ed4
Cody Baldwin <cody@lunaflare.com>
2015-07-19 19:02:30 -0400

    Base page progress
    base page is up using bootstrap

--- Swapp_app/app/views.py
+++ Swapp_app/app/views.py
@@ -1,6 +1,12 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	user = {'nickname': 'John'}
+	return render_template('index.html', user = user)
+
+@app.route('/login')
+def login():
+	return render_template('login.html')
